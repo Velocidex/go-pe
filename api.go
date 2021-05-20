c4ee60a0e249b1a4151b9556e9e5ac9f7a7374cf
Luca Guerra <54288318+luca-sd@users.noreply.github.com>
2021-05-20 17:57:35 +0200

    Handle null resource_base (#12)

--- api.go
+++ api.go
@@ -171,9 +171,13 @@ func GetVersionInformation(
 	resource_base int64) *ordereddict.Dict {
 	result := ordereddict.NewDict()
 
+	resourceDirectory := nt_header.ResourceDirectory(rva_resolver)
+	if resource_base == 0 {
+		resource_base = resourceDirectory.Offset
+	}
+
 	// Find the RT_VERSION resource.
-	for _, entry := range nt_header.
-		ResourceDirectory(rva_resolver).Entries() {
+	for _, entry := range resourceDirectory.Entries() {
 
 		if entry.NameString(resource_base) == "RT_VERSION" {
 			for _, child := range entry.Traverse(resource_base) {
