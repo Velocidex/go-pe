edb65d8401f5ec1d68cdf5e88b8f1e55a5a91b09
Mike Cohen <mike@velocidex.com>
2021-09-15 14:56:55 +1000

    Added more bounds checking to import table parsing.
    Limit the import table size to protect against corrupted PE files.

--- imports.go
+++ imports.go
@@ -11,6 +11,8 @@ func (self *IMAGE_NT_HEADERS) ImportDirectory(
 	result := []*IMAGE_IMPORT_DESCRIPTOR{}
 
 	dir := self.DataDirectory(IMAGE_DIRECTORY_ENTRY_IMPORT)
+	Debug(dir)
+
 	offset := rva_resolver.GetFileAddress(dir.VirtualAddress())
 	for offset > 0 {
 		desc := self.Profile.IMAGE_IMPORT_DESCRIPTOR(
@@ -18,6 +20,8 @@ func (self *IMAGE_NT_HEADERS) ImportDirectory(
 		if desc.Name() == 0 && desc.Characteristics() == 0 {
 			break
 		}
+
+		Debug(desc)
 
 		result = append(result, desc)
 		if len(result) > MAX_IMPORT_TABLE_LENGTH {
@@ -32,6 +36,9 @@ func (self *IMAGE_NT_HEADERS) ImportDirectory(
 
 func (self *IMAGE_IMPORT_DESCRIPTOR) DLLName(rva_resolver *RVAResolver) string {
 	offset := int64(rva_resolver.GetFileAddress(self.Name()))
+	if offset == 0 {
+		return ""
+	}
 	result := ParseTerminatedString(self.Reader, offset)
 	return result
 }
@@ -73,7 +80,6 @@ func (self *IMAGE_IMPORT_DESCRIPTOR) Functions32(rva_resolver *RVAResolver) []st
 
 func (self *IMAGE_IMPORT_DESCRIPTOR) Functions64(rva_resolver *RVAResolver) []string {
 	result := []string{}
-
 	offset := int64(rva_resolver.GetFileAddress(self.OriginalFirstThunk()))
 	for {
 		thunk := self.Profile.IMAGE_THUNK_DATA64(self.Reader, offset)
@@ -81,18 +87,35 @@ func (self *IMAGE_IMPORT_DESCRIPTOR) Functions64(rva_resolver *RVAResolver) []st
 			break
 		}
 
-		import_by_name := self.Profile.IMAGE_IMPORT_BY_NAME(
-			self.Reader, int64(rva_resolver.GetFileAddress(
-				uint32(thunk.AddressOfData()))))
-
-		if import_by_name.Offset != 0 {
-			result = append(result, import_by_name.Name())
-		} else {
-			// If the import is by ordinal then encode it
-			// as hex.
+		ordinal := thunk.Ordinal()
+		if ordinal&0x8000000000000000 > 0 {
+			// If the import is by ordinal then encode it as hex.
 			result = append(result, fmt.Sprintf(
 				"%#x", 0xFFFFFF&thunk.Ordinal()))
+
+		} else {
+			file_address := int64(rva_resolver.GetFileAddress(
+				uint32(thunk.AddressOfData())))
+
+			// If the thunk address is not found in the file skip this
+			// thunk.
+			if file_address == 0 {
+				return result
+			}
+
+			Debug(thunk)
+
+			import_by_name := self.Profile.IMAGE_IMPORT_BY_NAME(
+				self.Reader, file_address)
+
+			name := import_by_name.Name()
+			if name != "" {
+				if import_by_name.Offset != 0 {
+					result = append(result, name)
+				}
+			}
 		}
+
 		offset += int64(thunk.Size())
 		if len(result) > MAX_IMPORT_TABLE_LENGTH {
 			break
@@ -107,14 +130,25 @@ func GetImports(nt_header *IMAGE_NT_HEADERS, rva_resolver *RVAResolver) []string
 	for _, desc := range nt_header.ImportDirectory(rva_resolver) {
 		dll_name := desc.DLLName(rva_resolver)
 
+		// Is it valid to have an import with an empty dll name?
+		if dll_name == "" {
+			continue
+		}
+
 		if nt_header.OptionalHeader().Magic() == 0x20b {
 			for _, name := range desc.Functions64(rva_resolver) {
 				result = append(result, dll_name+"!"+name)
 			}
+
 		} else {
 			for _, name := range desc.Functions32(rva_resolver) {
 				result = append(result, dll_name+"!"+name)
 			}
+
+		}
+
+		if len(result) > MAX_IMPORT_TABLE_LENGTH {
+			break
 		}
 	}
 
